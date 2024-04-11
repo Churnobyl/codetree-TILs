fdfc01eb8dadad851c7b2f1549f6766e15093bbd
Churnobyl <77923267+Churnobyl@users.noreply.github.com>
2024-04-11 17:41:30 +0900

    Title: 코드트리 빵, Time: 145ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240411/코드트리 빵/codetree-mon-bread.java
+++ 240411/코드트리 빵/codetree-mon-bread.java
@@ -18,21 +18,20 @@ class Person {
 		return "Person [y=" + y + ", x=" + x + ", t=" + t + ", dest=" + Arrays.toString(dest) + "]";
 	}
 
-	
 }
 
 public class Main {
 	static int n, m, t;
 	static int[][] map;
 	static Queue<Person> queue = new LinkedList<>();
-	static int[] dy = {-1, 0, 0, 1};
-	static int[] dx = {0, -1, 1, 0};
-	
+	static int[] dy = { -1, 0, 0, 1 };
+	static int[] dx = { 0, -1, 1, 0 };
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
 		m = sc.nextInt();
-		
+
 		map = new int[n][n];
 
 		for (int i = 0; i < n; i++) {
@@ -40,87 +39,88 @@ public class Main {
 				map[i][j] = sc.nextInt();
 			}
 		}
-		
+
 		for (int i = 1; i < m + 1; i++) {
 			t++;
 			move();
 			enter(sc);
 		}
-		
+
 		while (!queue.isEmpty()) {
 			t++;
 			move();
 		}
-		
+
 		System.out.println(t);
 	}
 
 	private static void enter(Scanner sc) {
 		int y = sc.nextInt() - 1;
 		int x = sc.nextInt() - 1;
-		
+
 		int[][] dp = new int[n][n];
-		
+
 		for (int i = 0; i < n; i++) {
 			Arrays.fill(dp[i], Integer.MAX_VALUE);
 		}
-		
+
 		dp[y][x] = 0;
-		
+
 		int dist = Integer.MAX_VALUE;
 		int distY = Integer.MAX_VALUE, distX = Integer.MAX_VALUE;
-		
+
 		Queue<int[]> findQueue = new LinkedList<>();
-		findQueue.add(new int[] {y, x});
-		
+		findQueue.add(new int[] { y, x });
+
 		while (!findQueue.isEmpty()) {
 			int[] next = findQueue.poll();
-			
+
 			int ky = next[0];
 			int kx = next[1];
-			
-			if (map[ky][kx] < 0) continue;
-			
+
+			if (map[ky][kx] < 0 && map[ky][kx] > -t)
+				continue;
+
 			if (map[ky][kx] == 1) {
 				boolean isChange = false;
-				
+
 				if (dp[ky][kx] < dist) {
 					isChange = true;
 				} else if (dp[ky][kx] == dist) {
-					if (ky < distY) {						
+					if (ky < distY) {
 						isChange = true;
 					} else if (ky == distY) {
 						if (kx < distX) {
-							isChange = true;							
+							isChange = true;
 						}
 					}
 				}
-				
+
 				if (isChange) {
 					dist = dp[ky][kx];
 					distY = ky;
-					distX = kx;					
+					distX = kx;
 				}
 				continue;
 			}
-			
+
 			for (int i = 0; i < 4; i++) {
 				int ny = ky + dy[i];
 				int nx = kx + dx[i];
-				
+
 				if (0 <= ny && ny < n && 0 <= nx && nx < n) {
 					if (map[ny][nx] >= 0) {
 						if (dp[ky][kx] + 1 < dp[ny][nx]) {
 							dp[ny][nx] = dp[ky][kx] + 1;
-							findQueue.add(new int[] {ny, nx});							
+							findQueue.add(new int[] { ny, nx });
 						} else if (map[ny][nx] == 1 && dp[ky][kx] + 1 == dp[ny][nx]) {
-							findQueue.add(new int[] {ny, nx});
+							findQueue.add(new int[] { ny, nx });
 						}
 					}
 				}
 			}
 		}
-		
+
 		Person newPerson = new Person(distY, distX, t);
 		newPerson.dest[0] = y;
 		newPerson.dest[1] = x;
@@ -131,37 +131,88 @@ public class Main {
 	private static void move() {
 		while (!queue.isEmpty() && queue.peek().t < t) {
 			Person person = queue.poll();
-			
+
 			int dist = Integer.MAX_VALUE;
 			int distY = Integer.MAX_VALUE;
 			int distX = Integer.MAX_VALUE;
-			
+
+			boolean check = false;
+
 			for (int i = 0; i < 4; i++) {
 				int ny = person.y + dy[i];
 				int nx = person.x + dx[i];
-				
-				if (0 > ny || ny >= n || 0 > nx || nx >= n) continue;
-				
-				if (map[ny][nx] < 0) continue;
-				
-				int cache = Math.abs(person.dest[0] - ny) + Math.abs(person.dest[1] - nx);
-				
-				if (cache < dist) {
-					dist = cache;
+
+				if (0 > ny || ny >= n || 0 > nx || nx >= n)
+					continue;
+
+				if (map[ny][nx] < 0 && map[ny][nx] > -t) {
+					continue;
+
+				}
+
+				if (ny == person.dest[0] && nx == person.dest[1]) {
+					check = true;
+
+					map[ny][nx] = -t;
+					break;
+				}
+
+				int d = bfs(ny, nx, person.dest);
+
+				if (dist > d) {
+					dist = d;
 					distY = ny;
 					distX = nx;
 				}
 			}
-			
-			if (dist != 0) {
-				person.y = distY;
-				person.x = distX;
-				person.t++;
 
-				queue.add(person);
-			} else {
-				map[distY][distX] = -t;
+			if (check)
+				continue;
+
+			person.y = distY;
+			person.x = distX;
+			person.t++;
+			queue.add(person);
+		}
+
+	}
+
+	private static int bfs(int y, int x, int[] dest) {
+		int[][] dp = new int[n][n];
+
+		for (int i = 0; i < n; i++) {
+			Arrays.fill(dp[i], Integer.MAX_VALUE);
+		}
+
+		dp[y][x] = 0;
+
+		Queue<int[]> findQueue = new LinkedList<>();
+		findQueue.add(new int[] { y, x });
+
+		while (!findQueue.isEmpty()) {
+			int[] next = findQueue.poll();
+
+			int ky = next[0];
+			int kx = next[1];
+
+			if (map[ky][kx] < 0 || map[ky][kx] == 1)
+				continue;
+
+			for (int i = 0; i < 4; i++) {
+				int ny = ky + dy[i];
+				int nx = kx + dx[i];
+
+				if (0 <= ny && ny < n && 0 <= nx && nx < n) {
+					if (map[ny][nx] >= 0 || map[ny][nx] == -t) {
+						if (dp[ky][kx] + 1 < dp[ny][nx]) {
+							dp[ny][nx] = dp[ky][kx] + 1;
+							findQueue.add(new int[] { ny, nx });
+						}
+					}
+				}
 			}
 		}
+
+		return dp[dest[0]][dest[1]];
 	}
 }
